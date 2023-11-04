a03e325fd3e3e5fb5f06f5cdee771370f554204e
EDPankina <145845778+EDPankina@users.noreply.github.com>
2023-11-04 23:42:26 +0300

    Laba2

---
+++ laba2/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/vuz/OOP/laba2
+# Build directory: /mnt/d/vuz/OOP/laba2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(BASIC "/mnt/d/vuz/OOP/laba2/build/tests")
+set_tests_properties(BASIC PROPERTIES  _BACKTRACE_TRIPLES "/mnt/d/vuz/OOP/laba2/CMakeLists.txt;17;add_test;/mnt/d/vuz/OOP/laba2/CMakeLists.txt;0;")
