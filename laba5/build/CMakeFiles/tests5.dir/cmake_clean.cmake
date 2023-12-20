db7c9b1a9ff79716348d58ce507adbcd513301f7
EDPankina <145845778+EDPankina@users.noreply.github.com>
2023-12-20 21:28:11 +0300

    Add files via upload

---
+++ laba5/build/CMakeFiles/tests5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tests5.dir/tests/allocator_tests.cpp.o"
+  "CMakeFiles/tests5.dir/tests/allocator_tests.cpp.o.d"
+  "CMakeFiles/tests5.dir/tests/vector_tests.cpp.o"
+  "CMakeFiles/tests5.dir/tests/vector_tests.cpp.o.d"
+  "tests5"
+  "tests5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tests5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ laba5/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/d/vuz/OOP/laba5
+# Build directory: /mnt/d/vuz/OOP/laba5/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[LAB5]=] "/mnt/d/vuz/OOP/laba5/build/tests5")
+set_tests_properties([=[LAB5]=] PROPERTIES  _BACKTRACE_TRIPLES "/mnt/d/vuz/OOP/laba5/CMakeLists.txt;17;add_test;/mnt/d/vuz/OOP/laba5/CMakeLists.txt;0;")
